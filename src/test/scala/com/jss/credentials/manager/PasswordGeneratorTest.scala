0fe4d4e93f1f4a9c62074080f52c21bb34261d10
Jonatan Silva <jonatan.tab58@gmail.com>
2023-06-14 23:47:33 -0300

    ✅ Tests for proving password generator

--- src/test/scala/com/jss/credentials/manager/PasswordGeneratorTest.scala
+++ src/test/scala/com/jss/credentials/manager/PasswordGeneratorTest.scala
@@ -1,11 +1,41 @@
 package com.jss.credentials.manager
-import com.jss.credentials.manager.PasswordGenerator
+
+import java.util.regex.Pattern
+
 
 // For more information on writing tests, see
 // https://scalameta.org/munit/docs/getting-started.html
 class PasswordGeneratorTest extends munit.FunSuite {
-  test("Generate a password with length 10 successful") {
+  test("Generates a password with length equals to 10 successful") {
     val password = PasswordGenerator.generate(10)
     assertEquals(password.length, 10)
+  }
+
+  test("Password must contains at least one special character") {
+    val password = PasswordGenerator.generate(length=10, specials=true, numbers=true, lowercases=true, uppercases=true)
+    val pattern = Pattern.compile("[!@#$%&*()_+=|<>?{}\\[\\]~-]")
+    
+    assert(pattern.matcher(password).find())
+  }
+
+  test("Password must not contains special characters") {
+    val password = PasswordGenerator.generate(10, specials=false, numbers=true, lowercases=true, uppercases=true)
+    val pattern = Pattern.compile("[!@#$%&*()_+=|<>?{}\\[\\]~-]")
+    
+    assert(!pattern.matcher(password).find())
+  }
+
+  test("Password must contains only numbers") {
+    val password = PasswordGenerator.generate(10, specials=false, numbers=true, lowercases=false, uppercases=false)
+    val pattern = Pattern.compile("[0-9]")
+    
+    assert(pattern.matcher(password).find())
+  }
+
+  test("Password must contains only letters(camel case)") {
+    val password = PasswordGenerator.generate(10, specials=false, numbers=false, lowercases=true, uppercases=true)
+    val pattern = Pattern.compile("[a-zA-Z]")
+    print(password)
+    assert(pattern.matcher(password).find())
   }
 }
